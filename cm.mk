ba70693a96b8186d18185c58eb5e47b48dc6a6a3
Lalit Maganti <tilal6991@gmail.com>
2012-04-27 18:35:11 +0100

    Fix a couple of mistakes

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/zte/skate/skate.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
