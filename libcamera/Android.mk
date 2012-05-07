28c04aebf26aa535336df5c9d6edac48d5fa30b0
Lalit Maganti <lalitmaganti@gmail.com>
2012-05-07 20:33:05 +0200

    Update to match P500

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -38,18 +38,15 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_FLAGS        += -O3
-LOCAL_MODULE_TAGS    := optional
-LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE         := camera.$(TARGET_BOARD_PLATFORM)
-LOCAL_SRC_FILES      := cameraHal.cpp
+LOCAL_C_FLAGS += -O3
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_SRC_FILES := cameraHal.cpp
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware libcamera
-
-LOCAL_C_INCLUDES       := frameworks/base/services \
-                          frameworks/base/include \
-                          hardware/libhardware/include \
-                          hardware/qcom/display/libgralloc
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware libcamera libui
+LOCAL_C_INCLUDES := frameworks/base/services/ frameworks/base/include
+LOCAL_C_INCLUDES += hardware/libhardware/include/ hardware
 
 include $(BUILD_SHARED_LIBRARY)
