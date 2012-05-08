ab1143d7707acec8063884c4eb745762df71436b
Lalit Maganti <lalitmaganti@gmail.com>
2012-05-08 17:50:31 +0200

    Trying to update camera even further

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -38,15 +38,23 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_FLAGS += -O3
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
-LOCAL_SRC_FILES := cameraHal.cpp
-LOCAL_PRELINK_MODULE := false
+LOCAL_C_FLAGS          += -O3
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
 
-LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware libcamera libui
-LOCAL_C_INCLUDES := frameworks/base/services/ frameworks/base/include
-LOCAL_C_INCLUDES += hardware/libhardware/include/ hardware
+LOCAL_SRC_FILES        := QcomCamera.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
+                          libcutils libhardware libui libcamera
+
+LOCAL_C_INCLUDES       := frameworks/base/services \
+                          frameworks/base/include \
+                          hardware/libhardware/include
+
+LOCAL_CFLAGS           := -DPREVIEW_MSM7K
+LOCAL_C_INCLUDES       += hardware/qcom/display/libgralloc
+
+LOCAL_PRELINK_MODULE   := false
 
 include $(BUILD_SHARED_LIBRARY)
