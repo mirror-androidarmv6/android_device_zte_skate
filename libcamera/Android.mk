45bbfc546caab29dd60d3639d375ede0ef805314
Lalit Maganti <lalitmaganti@gmail.com>
2012-05-08 17:51:32 +0200

    Fix derp

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -43,7 +43,7 @@ LOCAL_MODULE_TAGS      := optional
 LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
 
-LOCAL_SRC_FILES        := QcomCamera.cpp
+LOCAL_SRC_FILES        := cameraHal.cpp
 
 LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
                           libcutils libhardware libui libcamera
