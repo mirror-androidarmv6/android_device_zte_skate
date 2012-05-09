5bbdd1acb8944585adf5b8a04721768a8fdf2935
Lalit Maganti <lalitmaganti@gmail.com>
2012-05-09 14:27:47 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -9,8 +9,11 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=skate BUILD_ID=IMM76D BUILD_FINGERPRINT=google/soju/crespo:4.0.4/IMM76D/299849:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.4 IMM76D 299849 release-keys" BUILD_NUMBER=299849
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_FINGERPRINT=google/soju/crespo:4.0.4/IMM76D/299849:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.4 IMM76D 299849 release-keys" BUILD_NUMBER=299849
 
 PRODUCT_NAME := cm_skate
 PRODUCT_DEVICE := skate
+PRODUCT_BRAND := ZTE
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_MODEL := Skate
 PRODUCT_RELEASE_NAME := Skate
