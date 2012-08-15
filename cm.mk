d59af3c04da32723685e534ca5025e371f2ff1f4
Lalit Maganti <lalitmaganti@gmail.com>
2012-08-15 15:05:57 -0700

    Some fixes

--- cm.mk
+++ cm.mk
@@ -1,13 +1,14 @@
+# Inherit device configuration
 $(call inherit-product, device/zte/skate/skate.mk)
 
 # Correct bootanimation size for the screen
 TARGET_BOOTANIMATION_NAME := vertical-480x800
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=skate BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB/atlas40:4.1.1/JRO03C/eng.songsy.20120718.233441:eng/test-keys PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.1 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
 
@@ -26,12 +27,6 @@ ifdef I_AM_TILAL
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.goo.board=skate \
    ro.goo.developerid=tilal6991 \
-   ro.goo.version=$(shell date +%Y%m%d)
-ifdef NO_PB
-PRODUCT_PROPERTY_OVERRIDES += \
-   ro.goo.rom=cfx2skateslow
-else
-PRODUCT_PROPERTY_OVERRIDES += \
+   ro.goo.version=$(shell date +%Y%m%d) \
    ro.goo.rom=cfx2skate
-endif
 endif
