515a5ca8faa090000381f2f12ff99e6342966547
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-23 16:56:50 +0000

    vendor: Switch to tiny config

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ TARGET_SCREEN_WIDTH := 480
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cm/config/tiny.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=skate BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB/atlas40:4.1.1/JRO03C/eng.songsy.20120718.233441:eng/test-keys PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.1 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
 
