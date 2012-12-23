741aaa0e9fb04370b61723390b557acef31d9865
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-23 22:55:32 +0000

    skate: bt specific
    Change-Id: Ia2bd8b524638bfd9edaeb76c2c1612d4ff91dfe3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ BOARD_USE_LEGACY_TOUCHSCREEN := true
 # Bluetooth
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := device/zte/skate/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/zte/skate/bluetooth
 
 # dev:    size   erasesize  name
 # mtd0: 00500000 00020000 "recovery"
