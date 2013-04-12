d23d65685b74110412b6f00ab307aafc45b64263
Lalit Maganti <lalitmaganti@gmail.com>
2013-04-12 20:10:42 +0200

    skate: update recovery files (device side)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,8 +19,9 @@ include device/zte/msm7x27-common/BoardConfigCommon.mk
 include vendor/zte/skate/BoardConfigVendor.mk
 
 # Kernel and target stuff
-TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/skate/prebuilt/recovery_kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/skate/recovery/recovery_kernel
 TARGET_RECOVERY_INITRC := device/zte/skate/recovery/recovery.rc
+TARGET_RECOVERY_FSTAB := device/zte/skate/recovery/recovery.fstab
 TARGET_KERNEL_CONFIG := skate_jb_defconfig
 BOARD_KERNEL_CMDLINE := androidboot.hardware=skate console=null
 TARGET_BOOTLOADER_NAME := skate
